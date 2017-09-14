ef5b8a2482342b71755f94085da5797d79dcaf1a
Nikita Subbot <nsubbot17@gmail.com>
2017-09-15 00:36:50 +0300

    First algorithm version realized (16/20)

---
+++ app.js
@@ -0,0 +1,11 @@
+let check = require("./src/index.js");
+
+const config1 = [['(', ')']];
+const config2 = [['(', ')'], ['[', ']']];
+const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
+const config4 = [['|', '|']];
+const config5 = [['(', ')'], ['|', '|']];
+const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
+const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
+
+check('|(|)', config5);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let sameInd;
+  
+  for(let i = 0; i < str.length; i ++ ) {
+    for(let j = 0; j < bracketsConfig.length; j++ ) {
+      if(str[i] === bracketsConfig[j][0])
+        stack.push(str[i]);
+      if((str[i] === bracketsConfig[j][1]) && (stack[stack.length-1] === bracketsConfig[j][0]))
+        stack.pop();
+    }
+  }
+  return stack.length === 0;
 }
